93adffe6a6304b6b3d78cd1abf5134cf59a654b2
Bartac <remy.mainard@orange.fr>
2017-03-13 15:59:26 +0100

    first commit

---
+++ src/io/avengers/ws/HeroResource.java
@@ -0,0 +1,24 @@
+package io.avengers.ws;
+
+import java.util.Set;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.avengers.domain.Hero;
+import io.avengers.service.HeroService;
+
+@Path("heroes")
+@Produces(MediaType.APPLICATION_JSON)
+public class HeroResource {
+	
+	@GET
+	public Set<Hero> getAllHeroes() {
+		
+		HeroService hService = new HeroService();
+		return hService.findAll();
+		
+	}
+}
---
+++ src/io/avengers/ws/MarvelApplication.java
@@ -0,0 +1,10 @@
+package io.avengers.ws;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("marvel")
+public class MarvelApplication extends Application{
+
+	
+}
