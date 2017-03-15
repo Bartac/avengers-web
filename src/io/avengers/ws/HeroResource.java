681f11bf47ab9699111019ec021d74c05a43815f
Bartac <remy.mainard@orange.fr>
2017-03-15 10:55:46 +0100

    COMMIT

--- src/io/avengers/ws/HeroResource.java
+++ src/io/avengers/ws/HeroResource.java
@@ -10,7 +10,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
 import io.avengers.domain.Hero;
 import io.avengers.service.HeroService;
