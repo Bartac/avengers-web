ca24d59ca54e3744600d47ca1c5a36e482a39656
jbog77 <bogota.jeremy@gmail.com>
2017-03-15 10:08:22 +0100

    Added Movie Method POST

--- src/io/avengers/ws/MovieResource.java
+++ src/io/avengers/ws/MovieResource.java
@@ -2,15 +2,16 @@ package io.avengers.ws;
 
 import java.util.Set;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import io.avengers.domain.Hero;
 import io.avengers.domain.Movie;
-import io.avengers.service.HeroService;
 import io.avengers.service.MovieService;
 
 @Path("movies")
@@ -31,5 +32,19 @@ public class MovieResource {
     public Movie findMovieById(@PathParam("id") String id) {
 		MovieService mService = new MovieService();
         return mService.findMoviesById(id);
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createMovie(Movie movie) throws Exception{
+    	MovieService mService = new MovieService();
+    	System.out.println(movie);
+    	if (movie.getName().isEmpty()){
+			return Response.status(406).entity("\"Name Empty\"").build();
+    	}
+    	
+    	mService.createMovie(movie.getName());
+		return Response.status(201).entity("\""+movie+"\"").build();
+		
     }
 }
